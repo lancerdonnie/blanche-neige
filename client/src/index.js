c73481c75aaccb8d385b106010c15f61a6208328
lancerdonnie <jideadedejifirst@gmail.com>
2020-02-03 12:34:25 +0100

    deployment changes

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-let basename = '/client';
-if (process.env.NODE_ENV !== 'production') {
-  basename = null;
-}
 ReactDOM.render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter basename='/client'>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
