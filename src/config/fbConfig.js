be7e44dc2d2567d37761f201fb216601e8593bf1
lancerdonnie <jideadedejifirst@gmail.com>
2020-02-03 15:52:00 +0100

    fixing env var

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,9 +5,9 @@ import 'firebase/auth';
 let key;
 if (process.env.NODE_ENV !== 'production') {
   key = process.env.REACT_APP_GOOGLE;
-  console.log('local', key);
 } else {
   key = process.env.GOOGLE;
+  console.log('env', process.env.GOOGLE, process.env);
   console.log('prod', key);
 }
 var firebaseConfig = {
